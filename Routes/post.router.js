9e27e82346f88ad027bb923e5175d9512e489a00
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-06-01 14:21:27 +0200

    Henter data fra postman

--- Routes/post.router.js
+++ Routes/post.router.js
@@ -3,8 +3,28 @@ import express from 'express';
 const postRouter = express.Router()
 
 
-postRouter.get('/post', (req, res)=>{
-     console.log('Hent alle  poster');
+postRouter.get('/', (req, res)=>{
+     console.log(req.query);
+     res.send("Henter all solcreme")
+})
+postRouter.post('/create', (req, res)=>{
+     res.send(`${req.body.Firstname}${req.body.Lastname}
+               ${req.body.Email}${req.body.Birthdate}
+               ${req.body.Adresse} ${req.body.Stilling}
+              ${req.body.Password}
+             `);
+     console.log('henter alle posters');
+})
+
+postRouter.put('/update', (req, res)=>{
+     res.send('update alle  poster');
+     console.log('Opdater alle posters');
+})
+
+
+postRouter.delete('/delete', (req, res)=>{
+     res.send('slet alle  poster');
+     console.log('detlete alle posters');
 })
 
 export { postRouter }
--- index.js
+++ index.js
@@ -1,44 +1,22 @@
 import express from "express";
+import dotenv from 'dotenv';
 import { postRouter } from './Routes/post.router.js';
+dotenv.config()
 
 const app = express();
+app.use(express.urlencoded({extended:true}))
 
 app.get("/", (req, res) => {
   res.send("Welcome to my node app!");
 });
 
-app.get("/about", (req, res) => {
-  res.send("You can get a defferint post");
-});
 
-app.post("/about", (req, res) => {
-  res.send("You can post here");
-});
+app.use("/post", postRouter)
 
-app.put("/about", (req, res) => {
-  res.send("You can update here");
-});
-
-app.delete("/about", (req, res) => {
-  res.send("You can delete here");
-});
-
-app.get("/products", (req, res) => {
-  res.send("What we offer");
-});
-
-app.get("/address", (req, res) => {
-  res.send("Where to find us");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("How to get in touch");
+app.use((req, res,) => {
+  res.status(404).send("The page was not found");
 });
 
 app.listen(4242, () => {
   console.log("The server is running on port 4242: http://localhost:4242/");
-});
-
-app.use((req, res, next) => {
-  res.status(404).send("The page was not found");
 });
